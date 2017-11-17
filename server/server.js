80b816c0f113330269ad31c7dd6290a4b07d273c
ayush17251 <ayushpesit@gmail.com>
2017-11-17 09:51:03 +0530

    this is final

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIo = require('socket.io');
 const http = require('http');
+const port = process.env.PORT || 3000;
 const publicpath= path.join(__dirname,'../public');
 
 //const port = process.env.PORT ||3000;
@@ -12,13 +13,21 @@ var io = socketIo(server);
 
 io.on('connection',(socket)=>{
   console.log('New User connected');
+  socket.on('disconnect',(socket)=>{
+       console.log('User got Disconnected');
+  });
+  socket.on('fromclient',(new_message)=>{
+      console.log(Date.now(),new_message);
+      io.emit('newMessage')
+  });
+  socket.emit('toclient',{
+      text:'I m good'
+  })
 });
 
-io.on('disconnect',(socket)=>
-  console.log('User got Disconnected');
-});
 
 
-server.listen(3000,()=>{
+
+server.listen(port,()=>{
   console.log('server is up');
 });
